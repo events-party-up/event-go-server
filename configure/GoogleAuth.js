f55d8434c21767dcdc1445381d233c00e2a24384
Quach Ha Chan Thanh <qhcthanh@gmail.com>
2017-03-10 01:15:09 +0700

    Update google signin

--- configure/GoogleAuth.js
+++ configure/GoogleAuth.js
@@ -1,17 +1,34 @@
 /**
  * Created by thanhqhc on 3/9/17.
  */
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var auth = new GoogleAuth;
 
-passport.use(new GoogleStrategy({
-        clientID: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback"
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        console.log(profile);
+var client = new auth.OAuth2('633133829509-91rk6671rtm60nqdg0tiqjvhuenme6no.apps.googleusercontent.com', '', '');
+
+function requestFacebookInfo(access_token) {
+
+    var rxObject = Rx.Observable.create(function (observer) {
+        client.verifyIdToken(
+            req.body.token,
+            [
+                '799105536867-8tcdqtmv7krhgfh4tat9ch1b226t4kc7.apps.googleusercontent.com',
+                '799105536867-mb2kn9vvcvcs2qqf6q9qqmj2rhbus82b.apps.googleusercontent.com',
+                '633133829509-91rk6671rtm60nqdg0tiqjvhuenme6no.apps.googleusercontent.com',
+                '633133829509-ljhkn3tfe9iodo0e8vmqdkk9e0eju73f.apps.googleusercontent.com'
+            ],
+            function(error, login) {
+                if (error) {
+                    observer.error(error);
+                } else {
+                    console.log("Login success: " + login);
+                }
+            }
+        )
     }
-));
 
-module.exports = passport;
+    return rxObject;
+}
+
+
+
+module.exports = requestFacebookInfo;

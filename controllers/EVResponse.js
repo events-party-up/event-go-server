edbff245c1034dac1d74d448eed8a37c708dbeba
Thanh Quach <qhcthanh@gmail.com>
2017-03-13 14:35:10 +0700

    Add verifiyAccesstoken and edit fail response to status code

--- controllers/EVResponse.js
+++ controllers/EVResponse.js
@@ -16,14 +16,22 @@ function ErrorResponse(res,code,error) {
   if (error == undefined) {
     error = "Error not found";
   }
+  res.status(code);
+  res.send(error);
+}
 
-  res.json({
-    'code': code,
-    'error': error
-  });
+var jwt_decode = require('jwt-decode');
+
+function verifiyAccessToken(req, key) {
+  // CHeck user_id decode jwt
+  var decode = jwt_decode(access_token);
+  var value = decode[key];
+
+  return value;
 }
 
 module.exports = {
   success: SuccessResponse,
   failure: ErrorResponse
+  verifiyAccessToken: verifiyAccessToken,
 }

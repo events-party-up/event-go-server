2a3efe54460bc7ef333452eb4c29cbd81950e610
Quach Ha Chan Thanh <qhcthanh@gmail.com>
2017-04-22 01:28:48 +0700

    [Server] Set limit size

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({ limit: '2mb' }))
 
 app.use(function (req, res, next) {
 
